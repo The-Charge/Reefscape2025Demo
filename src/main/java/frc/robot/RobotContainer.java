5bd01ceb06be7b93d469fe40ad3d1457f469d73a
derekriter <derekriter08@gmail.com>
2025-01-18 11:37:11 -0500

    Failed attempt to get robot centric working

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.swervedrive.drivebase.TeleopDrive;
@@ -33,20 +34,26 @@ public class RobotContainer {
     
     // int rotationXboxAxis = 4;
     
+    /*
+     * ======================
+     * Swerve
+     * ======================
+     */
     private final SwerveInputStream swerveInputFieldOriented = SwerveInputStream.of(
-    drivebase.getSwerveDrive(),
-    () -> -driverXbox.getLeftY(),
-    () -> -driverXbox.getLeftX()
+        drivebase.getSwerveDrive(),
+        () -> -driverXbox.getLeftY(),
+        () -> -driverXbox.getLeftX()
     ).withControllerRotationAxis(() -> -driverXbox.getRightX())
-    .deadband(SwerveConstants.DEADBAND)
-    .scaleTranslation(SwerveConstants.DRIVE_SPEED)
-    .scaleRotation(SwerveConstants.DRIVE_SPEED)
-    .allianceRelativeControl(true);
+        .deadband(SwerveConstants.DEADBAND)
+        .scaleTranslation(SwerveConstants.DRIVE_SPEED)
+        .scaleRotation(SwerveConstants.DRIVE_SPEED)
+        .allianceRelativeControl(true);
     private final SwerveInputStream swereveInputRobotOriented = swerveInputFieldOriented.copy()
-    .robotRelative(true)
-    .allianceRelativeControl(false);
+        .robotRelative(true)
+        .allianceRelativeControl(false);
     private final Command driveFieldOriented = drivebase.driveFieldOriented(swerveInputFieldOriented);
     private final Command driveRobotOriented = drivebase.driveFieldOriented(swereveInputRobotOriented);
+    public boolean isFieldCentric = true;
     
     public RobotContainer() {
         // if (RobotBase.isSimulation()) {
@@ -60,7 +67,7 @@ public class RobotContainer {
         //                               SwerveConstants.LEFT_X_DEADBAND),
         // () -> -driverXbox.getRawAxis(rotationXboxAxis));
         
-        drivebase.setDefaultCommand(driveFieldOriented);
+        drivebase.setDefaultCommand(isFieldCentric ? driveFieldOriented : driveRobotOriented);
         
         configureNamedCommands();
         configureBindings();
@@ -70,6 +77,15 @@ public class RobotContainer {
     private void configureBindings() {
         driverXbox.b().onTrue((Commands.runOnce(drivebase::zeroGyroWithAlliance)));
         driverXbox.x().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+        driverXbox.back().onTrue(new InstantCommand(() -> {
+            isFieldCentric = !isFieldCentric;
+
+            Command curr = drivebase.getCurrentCommand();
+            if(curr != null)
+                curr.cancel();
+
+            drivebase.setDefaultCommand(isFieldCentric ? driveFieldOriented : driveRobotOriented);
+        }));
         //driverXbox.b().whileTrue(
         //    drivebase.driveToPose(
         //        new Pose2d(new Translation2d(4, 4), Rotation2d.fromDegrees(0)))
