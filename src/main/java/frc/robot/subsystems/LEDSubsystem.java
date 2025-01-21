d2f204e0f540312113119c055783b57a5d42b95d
Derek Riter <derekriter08@gmail.com>
2025-01-20 21:01:14 -0500

    Test code works, needs to be soldered for a proper connection

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -2,13 +2,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.LEDConstants;
+import edu.wpi.first.units.Units;
 
 public class LEDSubsystem extends SubsystemBase {
     
     private AddressableLED leds;
     private AddressableLEDBuffer buff;
+    private LEDPattern rainbow;
 
     public LEDSubsystem() {
         leds = new AddressableLED(LEDConstants.ledPort);
@@ -16,14 +19,17 @@ public class LEDSubsystem extends SubsystemBase {
         
         leds.setLength(LEDConstants.ledLength);
 
-        buff.setRGB(1, 255, 0, 0);
+        // buff.setRGB(1, 255, 0, 0);
+        
+        rainbow = LEDPattern.rainbow(255, 255).scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(10));
+        rainbow.applyTo(buff);
 
-        leds.setData(buff);
         leds.start();
     }
 
     @Override
     public void periodic() {
-        
+        rainbow.applyTo(buff);
+        leds.setData(buff);
     }
 }
