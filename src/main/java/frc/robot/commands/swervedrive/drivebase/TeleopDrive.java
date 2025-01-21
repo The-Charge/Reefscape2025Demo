a6d479ca996b9facb900edd0112d80f82dcb2f54
Derek Riter <derekriter08@gmail.com>
2025-01-20 19:59:26 -0500

    TeleopDrive seems to be fine, but need to check if gyro reset is correct

--- src/main/java/frc/robot/commands/swervedrive/drivebase/TeleopDrive.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/TeleopDrive.java
@@ -4,45 +4,24 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.constants.SwerveConstants;
 import frc.robot.subsystems.SwerveSubsystem;
 import java.util.function.*;
 
 public class TeleopDrive extends Command {
     
     private final SwerveSubsystem swerve;
-    private final DoubleSupplier vX;
-    private final DoubleSupplier vY;
+    private final DoubleSupplier vX, vY;
     private final DoubleSupplier heading;
     private final BooleanSupplier povUp, povLeft, povDown, povRight;
     private final BooleanSupplier centricToggle;
-    private double rotationSpeed;
-    private boolean usePOV;
+    private final BooleanSupplier shiftHalf, shiftQuarter;
+
+    private boolean usePOV = false;
     private boolean isFieldCentric = true;
+    private boolean centricToggleLast = false;
     
-    /**
-    * Used to drive a swerve robot in full field-centric mode. vX and vY supply
-    * translation inputs, where x is
-    * torwards/away from alliance wall and y is left/right. headingHorzontal and
-    * headingVertical are the Cartesian
-    * coordinates from which the robot's angle will be derived— they will be
-    * converted to a polar angle, which the robot
-    * will rotate to.
-    *
-    * @param swerve  The swerve drivebase subsystem.
-    * @param vX      DoubleSupplier that supplies the x-translation joystick input.
-    *                Should be in the range -1 to 1 with
-    *                deadband already accounted for. Positive X is away from the
-    *                alliance wall.
-    * @param vY      DoubleSupplier that supplies the y-translation joystick input.
-    *                Should be in the range -1 to 1 with
-    *                deadband already accounted for. Positive Y is towards the left
-    *                wall when looking through the driver
-    *                station glass.
-    * @param heading DoubleSupplier that supplies the robot's heading angle.
-    */
-    public TeleopDrive(SwerveSubsystem swerve, DoubleSupplier vX, DoubleSupplier vY,
-    DoubleSupplier heading, BooleanSupplier povUp, BooleanSupplier povLeft, BooleanSupplier povDown, BooleanSupplier povRight,
-    BooleanSupplier centricToggle) {
+    public TeleopDrive(SwerveSubsystem swerve, DoubleSupplier vX, DoubleSupplier vY, DoubleSupplier heading, BooleanSupplier povUp, BooleanSupplier povLeft, BooleanSupplier povDown, BooleanSupplier povRight, BooleanSupplier centricToggle, BooleanSupplier shiftHalf, BooleanSupplier shiftQuarter) {
         this.swerve = swerve;
         this.vX = vX;
         this.vY = vY;
@@ -52,77 +31,81 @@ public class TeleopDrive extends Command {
         this.povDown = povDown;
         this.povRight = povRight;
         this.centricToggle = centricToggle;
-        
-        rotationSpeed = 0;
+        this.shiftHalf = shiftHalf;
+        this.shiftQuarter = shiftQuarter;
         
         addRequirements(swerve);
     }
     
     @Override
-    public void initialize() {
-        usePOV = false;
-    }
+    public void initialize() {}
     
-    // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
         double headingX = 0;
         double headingY = 0;
         
-
-        
-        if (povUp.getAsBoolean())  {
+        if(povUp.getAsBoolean())  {
             usePOV = true;
             headingY = 1;
-        } else if (povLeft.getAsBoolean()) {
+        }
+        else if(povLeft.getAsBoolean()) {
             usePOV = true;
             headingX = 1;
-        } else if (povDown.getAsBoolean()) {
+        }
+        else if(povDown.getAsBoolean()) {
             usePOV = true;
             headingY = -1;
-        } else if(povRight.getAsBoolean()) {
+        }
+        else if(povRight.getAsBoolean()) {
             usePOV = true;
             headingX = -1;
         }
-        
-        if (Math.abs(heading.getAsDouble()) > swerve.getSwerveController().config.angleJoyStickRadiusDeadband) {
-            rotationSpeed = heading.getAsDouble() * swerve.getSwerveController().config.maxAngularVelocity;
-            usePOV = false;
-        } else {
-            rotationSpeed = 0;
-        }
-        
-        ChassisSpeeds desiredSpeeds = swerve.getTargetSpeeds(vX.getAsDouble(), vY.getAsDouble(), headingX, headingY);
-        //ChassisSpeeds desiredSpeeds = swerve.getTargetSpeeds(vX.getAsDouble(), vY.getAsDouble(), new Rotation2d(swerve.getHeading().getRadians()));
-        if (centricToggle.getAsBoolean()) {
+
+        if(centricToggle.getAsBoolean() && !centricToggleLast) {
             isFieldCentric = !isFieldCentric;
         }
-        // Limit velocity to prevent tippy
-        Translation2d translation = new Translation2d(vX.getAsDouble()*14.5, vY.getAsDouble()*14.5);
-        //translation = SwerveMath.limitVelocity(translation, swerve.getFieldVelocity(), swerve.getPose(),
-        //   Constants.LOOP_TIME, Constants.ROBOT_MASS, List.of(Constants.CHASSIS),
-        //  swerve.getSwerveDriveConfiguration());
+        centricToggleLast = centricToggle.getAsBoolean();
+
+        double shiftScalar = 1;
+        if(shiftQuarter.getAsBoolean())
+            shiftScalar = 0.25;
+        else if(shiftHalf.getAsBoolean())
+            shiftScalar = 0.5;
+        
+        double rotationSpeed = 0;
+        if(heading.getAsDouble() != 0) {
+            rotationSpeed = heading.getAsDouble() * swerve.getSwerveController().config.maxAngularVelocity;
+            usePOV = false;
+
+            rotationSpeed *= shiftScalar;
+            rotationSpeed *= SwerveConstants.DRIVE_SPEED;
+        }
+        
+        Translation2d translation = new Translation2d(vX.getAsDouble(), vY.getAsDouble())
+            .times(14.5) //Limit velocity
+            .times(shiftScalar) //trigger shifting scalar
+            .times(SwerveConstants.DRIVE_SPEED); //scale by drive speed percentage
+
+        //pov is not effected by drive speed percentage or trigger shifting
+        ChassisSpeeds povSpeeds = swerve.getTargetSpeeds(vX.getAsDouble(), vY.getAsDouble(), headingX, headingY);
+
+        //translation = SwerveMath.limitVelocity(translation, swerve.getFieldVelocity(), swerve.getPose(), Constants.LOOP_TIME, Constants.ROBOT_MASS, List.of(Constants.CHASSIS), swerve.getSwerveDriveConfiguration());
         //SmartDashboard.putNumber("LimitedTranslation", translation.getX());
         //SmartDashboard.putString("Translation", translation.toString());
         
-
-
-        
         // Make the robot move
-        if (usePOV) {
-            swerve.drive(translation, desiredSpeeds.omegaRadiansPerSecond, isFieldCentric);
-        } else {
+        if(usePOV) {
+            swerve.drive(translation, povSpeeds.omegaRadiansPerSecond, isFieldCentric);
+        }
+        else {
             swerve.drive(translation, rotationSpeed, isFieldCentric);
         }
-        // swerve.drive(translation, rotationSpeed, isFieldCentric);
-
-
     }
     
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {
-    }
+    public void end(boolean interrupted) {}
     
     // Returns true when the command should end.
     @Override
