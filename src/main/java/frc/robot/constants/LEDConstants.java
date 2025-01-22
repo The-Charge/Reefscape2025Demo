e538b7309a319db2d289128bcb70860ae3367625
Derek Riter <derekriter08@gmail.com>
2025-01-21 19:21:26 -0500

    Got everything working for leds, just need to have freshies program

--- src/main/java/frc/robot/constants/LEDConstants.java
+++ src/main/java/frc/robot/constants/LEDConstants.java
@@ -1,7 +1,31 @@
 package frc.robot.constants;
 
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.subsystems.LEDSubsystem;
+
 public abstract class LEDConstants {
     
     public static final int ledPort = 0;
-    public static final int ledLength = 32;
+    public static final int ledLength = 44;
+    public static final boolean swapRedGreen = true; //use if red and green channels need to be swapped
+
+    public static final LEDPattern colorTest = LEDPattern.steps(Map.ofEntries(
+        Map.entry(0d, LEDSubsystem.modColor(new Color("#ff0000"))),
+        Map.entry(1 / 12.0, LEDSubsystem.modColor(new Color("#ff8800"))),
+        Map.entry(2 / 12.0, LEDSubsystem.modColor(new Color("#ffff00"))),
+        Map.entry(3 / 12.0, LEDSubsystem.modColor(new Color("#88ff00"))),
+        Map.entry(4 / 12.0, LEDSubsystem.modColor(new Color("#00ff00"))),
+        Map.entry(5 / 12.0, LEDSubsystem.modColor(new Color("#00ff88"))),
+        Map.entry(6 / 12.0, LEDSubsystem.modColor(new Color("#00ffff"))),
+        Map.entry(7 / 12.0, LEDSubsystem.modColor(new Color("#0088ff"))),
+        Map.entry(8 / 12.0, LEDSubsystem.modColor(new Color("#0000ff"))),
+        Map.entry(9 / 12.0, LEDSubsystem.modColor(new Color("#8800ff"))),
+        Map.entry(10 / 12.0, LEDSubsystem.modColor(new Color("#ff00ff"))),
+        Map.entry(11 / 12.0, LEDSubsystem.modColor(new Color("#ff0088")))
+    ));
+    public static final Color chargeGreen = new Color("#008800");
+    public static final Color chargeGold = new Color("#ffaa00");
 }
--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -3,15 +3,20 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.LEDConstants;
+
+import java.security.KeyStore.Entry;
+import java.util.Map;
+
 import edu.wpi.first.units.Units;
 
 public class LEDSubsystem extends SubsystemBase {
     
     private AddressableLED leds;
     private AddressableLEDBuffer buff;
-    private LEDPattern rainbow;
+    private LEDPattern effect;
 
     public LEDSubsystem() {
         leds = new AddressableLED(LEDConstants.ledPort);
@@ -19,17 +24,25 @@ public class LEDSubsystem extends SubsystemBase {
         
         leds.setLength(LEDConstants.ledLength);
 
-        // buff.setRGB(1, 255, 0, 0);
-        
-        rainbow = LEDPattern.rainbow(255, 255).scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(10));
-        rainbow.applyTo(buff);
+        // effect = LEDPattern.rainbow(255, 255)
+        //     .scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(25));
+        effect = LEDPattern.steps(Map.of(0, modColor(LEDConstants.chargeGreen), 0.5, modColor(LEDConstants.chargeGold)))
+            .scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(25));
+        effect.applyTo(buff);
 
         leds.start();
     }
 
     @Override
     public void periodic() {
-        rainbow.applyTo(buff);
+        effect.applyTo(buff);
         leds.setData(buff);
+    }
+
+    public static Color modColor(Color in) {
+        if(!LEDConstants.swapRedGreen)
+            return in;
+        
+        return new Color(in.green, in.red, in.blue);
     }
 }
