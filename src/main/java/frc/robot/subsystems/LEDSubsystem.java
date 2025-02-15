57dd7231a0523fd2e57e2e1ff10dadd9adc7267f
Derek Riter <derekriter08@gmail.com>
2025-02-15 13:57:08 -0500

    Made a scan animation and implemented new color order

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,48 +1,61 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLED.ColorOrder;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.LEDConstants;
-
-import java.security.KeyStore.Entry;
-import java.util.Map;
-
-import edu.wpi.first.units.Units;
 
 public class LEDSubsystem extends SubsystemBase {
     
     private AddressableLED leds;
     private AddressableLEDBuffer buff;
-    private LEDPattern effect;
+    // private LEDPattern effect;
+    private int time = 0;
 
     public LEDSubsystem() {
         leds = new AddressableLED(LEDConstants.ledPort);
         buff = new AddressableLEDBuffer(LEDConstants.ledLength);
         
         leds.setLength(LEDConstants.ledLength);
+        leds.setColorOrder(ColorOrder.kRGB);
 
         // effect = LEDPattern.rainbow(255, 255)
         //     .scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(25));
-        effect = LEDPattern.steps(Map.of(0, modColor(LEDConstants.chargeGreen), 0.5, modColor(LEDConstants.chargeGold)))
-            .scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(25));
-        effect.applyTo(buff);
+        // effect = LEDPattern.steps(Map.of(0, modColor(LEDConstants.chargeGreen), 0.5, modColor(LEDConstants.chargeGold)))
+        //     .scrollAtRelativeSpeed(Units.Percent.per(Units.Second).of(25));
+        // effect.applyTo(buff);
 
         leds.start();
     }
 
     @Override
     public void periodic() {
-        effect.applyTo(buff);
-        leds.setData(buff);
-    }
+        // effect.applyTo(buff);
 
-    public static Color modColor(Color in) {
-        if(!LEDConstants.swapRedGreen)
-            return in;
-        
-        return new Color(in.green, in.red, in.blue);
+        final double width = 10;
+        final double speed = 1;
+        final Color col = LEDConstants.chargeGreen;
+
+        double offset = ((Math.sin((Math.PI * time * speed) / (buff.getLength() - width) - 0.5 * Math.PI) + 1) * 0.5) * (buff.getLength() - width);
+        for(int i = 0; i < buff.getLength(); i++) {
+            double strength = 0;
+            if(offset < i && i < width + offset) {
+                strength = Math.sin(Math.PI * (i - offset) / width);
+            }
+
+            Color pixel = new Color(
+                col.red * strength,
+                col.green * strength,
+                col.blue * strength
+            );
+
+            buff.setLED(i, pixel);
+        }
+
+        leds.setData(buff);
+
+        time++;
     }
 }
