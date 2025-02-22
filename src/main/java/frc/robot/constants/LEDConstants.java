e2e782d85900a9703c23d95bb03c05de23421e33
Derek Riter <derekriter08@gmail.com>
2025-02-22 15:41:03 -0500

    Remove unneeded color test

--- src/main/java/frc/robot/constants/LEDConstants.java
+++ src/main/java/frc/robot/constants/LEDConstants.java
@@ -15,20 +15,6 @@ public abstract class LEDConstants {
     public static final ColorOrder ledColorOrder = ColorOrder.kGRB;
     public static final Distance ledSpacing = Units.Meters.of(1 / 60.0); //60 leds per meter
 
-    public static final LEDPattern colorTest = LEDPattern.steps(Map.ofEntries(
-        Map.entry(0d, new Color("#ff0000")),
-        Map.entry(1 / 12.0, new Color("#ff8800")),
-        Map.entry(2 / 12.0, new Color("#ffff00")),
-        Map.entry(3 / 12.0, new Color("#88ff00")),
-        Map.entry(4 / 12.0, new Color("#00ff00")),
-        Map.entry(5 / 12.0, new Color("#00ff88")),
-        Map.entry(6 / 12.0, new Color("#00ffff")),
-        Map.entry(7 / 12.0, new Color("#0088ff")),
-        Map.entry(8 / 12.0, new Color("#0000ff")),
-        Map.entry(9 / 12.0, new Color("#8800ff")),
-        Map.entry(10 / 12.0, new Color("#ff00ff")),
-        Map.entry(11 / 12.0, new Color("#ff0088"))
-    ));
     public static final Color chargeGreen = new Color("#008800");
     public static final Color chargeGold = new Color("#ffaa00");
 }
