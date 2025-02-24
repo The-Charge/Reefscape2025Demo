849fa2129247c0deaff1565c51d96cbecd698e82
secretjameser <dvjamest@gmail.com>
2025-02-24 18:40:17 -0500

    cleaned up unnecessary code

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -23,8 +23,7 @@ import limelight.structures.LimelightSettings.LEDMode;
 public class VisionSubsystem extends SubsystemBase{
     SwerveSubsystem swerve;
 
-    Limelight limelight;
-    LimelightPoseEstimator llPoseEstimator;
+    //private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
 
     public String ll_name;
     RawFiducial[] fiducials;
@@ -50,13 +49,7 @@ public class VisionSubsystem extends SubsystemBase{
       this.swerve = swerve;
       this.ll_name = ll_name;
 
-      //Setup YALL limelight object (maybe should be in initialize)
-      limelight = new Limelight(ll_name);
-      NetworkTableInstance.getDefault().getTable(ll_name).getEntry("pipeline").setNumber(1);
-      limelight.getSettings().withLimelightLEDMode(LEDMode.PipelineControl).withCameraOffset(cameraOffset).save();
-      llPoseEstimator = limelight.getPoseEstimator(true);
-
-      //Setup PIDControllers for AlignToTag
+      // Setup PIDControllers for AlignToTag
       sideController = new PIDController(.4, 0.001, 0.0001);
       sideController.setSetpoint(0.0);
       sideController.setTolerance(ApriltagConstants.TRANSLATION_SIDE_POSE_TOLERANCE);
@@ -64,13 +57,15 @@ public class VisionSubsystem extends SubsystemBase{
       frontController = new PIDController(.4, 0.001, 0.0001);
       frontController.setSetpoint(0.0);
       frontController.setTolerance(ApriltagConstants.TRANSLATION_FRONT_POSE_TOLERANCE);
+
+      NetworkTableInstance.getDefault().getTable(ll_name).getEntry("pipeline").setNumber(1);
     }
 
     @Override
     public void periodic(){
       updateLimelightTracking();
-      UpdateLocalization();
       UpdateAlignerPID();
+      // UpdateLocalization();
     }
   
     public void updateLimelightTracking(){ 
@@ -114,52 +109,52 @@ public class VisionSubsystem extends SubsystemBase{
       return timeStamp;
     }
    
-    public void UpdateLocalization() {
-      NetworkTable table = NetworkTableInstance.getDefault().getTable(ll_name);
+    // public void UpdateLocalization() {
+    //   NetworkTable table = NetworkTableInstance.getDefault().getTable(ll_name);
 
-      double yaw = swerve.getHeading().getDegrees();
-      double yawRate = swerve.getSwerveDrive().getGyro().getYawAngularVelocity().in(DegreesPerSecond);
-      Double[] poseArray = {yaw, yawRate, 0., 0., 0., 0.};
-      table.getEntry("robot_orientation_set").setDoubleArray(poseArray);
+    //   double yaw = swerve.getHeading().getDegrees();
+    //   double yawRate = swerve.getSwerveDrive().getGyro().getYawAngularVelocity().in(DegreesPerSecond);
+    //   Double[] poseArray = {yaw, yawRate, 0., 0., 0., 0.};
+    //   table.getEntry("robot_orientation_set").setDoubleArray(poseArray);
 
-      Double[] rawFiducials = table.getEntry("rawfiducials").getDoubleArray(new Double[] { });
-      if (rawFiducials.length == 0) {
-        SmartDashboard.putBoolean(ll_name + " Pose Estimated ", false);
-        return;
-      }
+    //   Double[] rawFiducials = table.getEntry("rawfiducials").getDoubleArray(new Double[] { });
+    //   if (rawFiducials.length == 0) {
+    //     SmartDashboard.putBoolean(ll_name + " Pose Estimated ", false);
+    //     return;
+    //   }
 
-      double avgAmbig = 0;
-      int rawFiducialsLength = rawFiducials.length / 7;
-      for (int i = 1; i <= rawFiducialsLength; i++) {
-        SmartDashboard.putNumber(ll_name + " TagID #" + i, rawFiducials[i * 7 - 7]);
-        SmartDashboard.putNumber(ll_name + " TagX #" + i, rawFiducials[i * 7 - 6]);
-        SmartDashboard.putNumber(ll_name + " TagY #" + i, rawFiducials[i * 7 - 5]);
-        SmartDashboard.putNumber(ll_name + " Distance to tag #" + i, rawFiducials[i * 7 - 3]); // tag to camera
-        SmartDashboard.putNumber(ll_name + " Ambiguity #" + i, rawFiducials[i * 7 - 1]);
+    //   double avgAmbig = 0;
+    //   int rawFiducialsLength = rawFiducials.length / 7;
+    //   for (int i = 1; i <= rawFiducialsLength; i++) {
+    //     SmartDashboard.putNumber(ll_name + " TagID #" + i, rawFiducials[i * 7 - 7]);
+    //     SmartDashboard.putNumber(ll_name + " TagX #" + i, rawFiducials[i * 7 - 6]);
+    //     SmartDashboard.putNumber(ll_name + " TagY #" + i, rawFiducials[i * 7 - 5]);
+    //     SmartDashboard.putNumber(ll_name + " Distance to tag #" + i, rawFiducials[i * 7 - 3]); // tag to camera
+    //     SmartDashboard.putNumber(ll_name + " Ambiguity #" + i, rawFiducials[i * 7 - 1]);
 
-        avgAmbig += rawFiducials[i * 7 - 1] / rawFiducialsLength;
-      }
+    //     avgAmbig += rawFiducials[i * 7 - 1] / rawFiducialsLength;
+    //   }
 
-      SmartDashboard.putNumber("avg ambigg " + ll_name, avgAmbig);
+    //   SmartDashboard.putNumber("avg ambigg " + ll_name, avgAmbig);
 
-      if (avgAmbig > 0.5) {
-        SmartDashboard.putBoolean(ll_name + " Pose Estimated ", false);
-        return;
-      }
+    //   if (avgAmbig > 0.5) {
+    //     SmartDashboard.putBoolean(ll_name + " Pose Estimated ", false);
+    //     return;
+    //   }
 
-      Double[] botPoseArray = table.getEntry("botpose_orb_wpiblue").getDoubleArray(new Double[] { });
+    //   Double[] botPoseArray = table.getEntry("botpose_orb_wpiblue").getDoubleArray(new Double[] { });
       
-      if (botPoseArray.length == 0) {
-        SmartDashboard.putBoolean(ll_name + " Pose Estimated ", false);
-        return;
-      }
+    //   if (botPoseArray.length == 0) {
+    //     SmartDashboard.putBoolean(ll_name + " Pose Estimated ", false);
+    //     return;
+    //   }
 
-      double timeStamp = Timer.getFPGATimestamp() - Units.millisecondsToSeconds(botPoseArray[6]);
-      Pose2d pose = new Pose2d(botPoseArray[0], botPoseArray[1], new Rotation2d(Units.degreesToRadians(botPoseArray[5])));
-      // swerve.addVisionReading(pose, timeStamp);
+    //   double timeStamp = Timer.getFPGATimestamp() - Units.millisecondsToSeconds(botPoseArray[6]);
+    //   Pose2d pose = new Pose2d(botPoseArray[0], botPoseArray[1], new Rotation2d(Units.degreesToRadians(botPoseArray[5])));
+    //   swerve.addVisionReading(pose, timeStamp);
 
-      SmartDashboard.putBoolean(ll_name + " Pose Estimated ", true);
-    }
+    //   SmartDashboard.putBoolean(ll_name + " Pose Estimated ", true);
+    // }
 
     public void UpdateAlignerPID(){
       sideController.reset();
@@ -169,14 +164,6 @@ public class VisionSubsystem extends SubsystemBase{
       rightSideAdjust = MathUtil.clamp(sideController.calculate(txnc + ApriltagConstants.RIGHT_ALIGN_OFFSET,0), -0.2, 0.2);
       frontAdjust = MathUtil.clamp(frontController.calculate(distToRobot, ApriltagConstants.APRILTAG_POSE_OFFSET), -0.5, 0.5);
     }
-
-    public boolean robotRotationWithinThreshold(int tagid){
-      return 
-        Math.abs(ApriltagConstants.TAG_POSES[tagid].getRotation().getAngle() * 180 / Math.PI - 180
-        - llPoseEstimator.getPoseEstimate().get().pose.toPose2d().getRotation().getDegrees())
-        < ApriltagConstants.ANGLE_POSE_TOLERANCE;
-    }
-
   
     public boolean getObjectedDetected(){
       if (txnc != 0.0) return true;
