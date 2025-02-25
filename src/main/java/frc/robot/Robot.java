169c4b4d3ef7c434d2f147161aa7a39cd7d1f18d
ecmason <36716725+ecmason@users.noreply.github.com>
2025-02-25 13:33:18 -0500

    Bring Over Fix from YAGSL Example

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -101,6 +101,7 @@ public class Robot extends TimedRobot {
         {
             m_robotContainer.setMotorBrake(false);
             disabledTimer.stop();
+            disabledTimer.reset();
         }
     }
     
