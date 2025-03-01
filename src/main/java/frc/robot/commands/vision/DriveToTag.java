24a7e2f597bd80262a6f49992e10fb462365cf07
secretjameser <dvjamest@gmail.com>
2025-03-01 11:21:07 -0500

    pathplanner

--- src/main/java/frc/robot/commands/vision/DriveToTag.java
+++ src/main/java/frc/robot/commands/vision/DriveToTag.java
@@ -2,6 +2,7 @@ package frc.robot.commands.vision;
 
 import java.util.List;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
@@ -61,6 +62,7 @@ public class DriveToTag extends InstantCommand {
     rot2d = rot2d.minus(Rotation2d.k180deg);
 // keep stash, subtract 180 from rot, use getRot.to2d as the final rot
     List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
+      swerve.getPose(),
       new Pose2d(x, y, rot2d)
     );
 
@@ -73,6 +75,8 @@ public class DriveToTag extends InstantCommand {
     path.preventFlipping = true;
 
     // run the path
+    this.drivetoPose = AutoBuilder.followPath(path);
+    this.drivetoPose.schedule();
 
     SmartDashboard.putNumber("tag x", x);
     SmartDashboard.putNumber("tag y", y);
@@ -81,40 +85,9 @@ public class DriveToTag extends InstantCommand {
     intendedPose = new Pose2d(x,y,rot2d);
     
     drivetoPose = swerve.driveToPose(intendedPose);
-    drivetoPose.schedule();
+    // drivetoPose.schedule();
   }
    
-  /*
-   * // Create a list of waypoints from poses. Each pose represents one waypoint.
-   * // The rotation component of the pose should be the direction of travel. Do
-   * not use holonomic rotation.
-   * List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
-   * new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
-   * new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
-   * new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90))
-   * );
-   * 
-   * PathConstraints constraints = new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 *
-   * Math.PI); // The constraints for this path.
-   * // PathConstraints constraints = PathConstraints.unlimitedConstraints(12.0);
-   * // You can also use unlimited constraints, only limited by motor torque and
-   * nominal battery voltage
-   * 
-   * // Create the path using the waypoints created above
-   * PathPlannerPath path = new PathPlannerPath(
-   * waypoints,
-   * constraints,
-   * null, // The ideal starting state, this is only relevant for pre-planned
-   * paths, so can be null for on-the-fly paths.
-   * new GoalEndState(0.0, Rotation2d.fromDegrees(-90)) // Goal end state. You can
-   * set a holonomic rotation here. If using a differential drivetrain, the
-   * rotation will have no effect.
-   * );
-   * 
-   * // Prevent the path from being flipped if the coordinates are already correct
-   * path.preventFlipping = true;
-   */
-
   @Override 
   public void execute() {
     
@@ -127,6 +100,6 @@ public class DriveToTag extends InstantCommand {
 
   @Override
   public boolean isFinished() {
-    return drivetoPose != null;
+    return drivetoPose.isFinished();
   }
 }
